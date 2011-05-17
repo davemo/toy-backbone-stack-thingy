24d774e52690ad6f0314a7d54d437015589a2f55
Justin Searls <searls@gmail.com>
2011-05-16 20:57:58 -0400

    added basic sliding animations... would prefer it to slide like a microfiche

--- stack.js
+++ stack.js
@@ -9,21 +9,23 @@
 		};
 
 		var push = function(view) {
-			var $views = findViews();
+			var $views = findViews(),
+					newContainerId = config.containerId+'-view-'+$views.length;
+			
+			
 			$views.last().hide();
-
-			var newContainerId = config.containerId+'-view-'+$views.length;
-			$('<div id="'+newContainerId+'" class="stack-view"></div>').appendTo($container);    
-
+			var $new =$('<div id="'+newContainerId+'" class="stack-view"></div>').hide().appendTo($container);
 			view.view.apply(this,[$.extend(view.config,{ 
 				containerId: newContainerId 
-			})]);
+			})]);						
+			$new.show('slide',{direction:'right'},500)
+			
 			$.publish('stack/push');
 		};
 
 		var pop = function() {
 			findViews().last().remove();
-			findViews().last().show();				
+			findViews().last().show('slide',{direction:'left'},500);
 			$.publish('stack/pop');
 		}
 		
@@ -52,32 +54,31 @@
 		push(config.top)    
 	};
 
-	var makeLinks = function($into,name,number) {
+	WidgetA = function(config) {
+		var $into = $('#'+config.containerId).append('<h1>Welcome to Widget A!</h1>');
+		appendLinks($into,'B',3);
+	}
+
+	WidgetB = function(config) {
+		var $into = $('#'+config.containerId).append('<h2>Welcome to Widget B!</h2>');    
+		appendLinks($into,'C',7)
+	}
+
+	WidgetC = function(config) {
+		var $into = $('#'+config.containerId).append('<h3>Welcome to Widget C!</h3>');    
+		appendLinks($into,'D',123)
+	}
+
+	WidgetD = function(config) {
+		var $into = $('#'+config.containerId).append('<h4>Welcome to Widget D!</h4>');    
+		appendLinks($into,'A',1)
+	}
+
+	var appendLinks = function($into,name,number) {
 		for(var i=0;i<number;i++) {
 			var $link = $('<a href="#" class="stack-push">Go to '+name+' #'+i+'</a>').appendTo($into);
 			$link.data('stack',{ view: window['Widget'+name], config: {id: i }  });
 			$link.wrap('<li></li>');
 		}
 	};
-
-	WidgetA = function(config) {
-		var $into = $('#'+config.containerId).append('<h1>Welcome to Widget A!</h1>');
-		makeLinks($into,'B',3);
-	}
-
-	WidgetB = function(config) {
-		var $into = $('#'+config.containerId).append('<h2>Welcome to Widget B!</h2>');    
-		makeLinks($into,'C',7)
-	}
-
-	WidgetC = function(config) {
-		var $into = $('#'+config.containerId).append('<h3>Welcome to Widget C!</h3>');    
-		makeLinks($into,'D',123)
-	}
-
-	WidgetD = function(config) {
-		var $into = $('#'+config.containerId).append('<h4>Welcome to Widget D!</h4>');    
-		makeLinks($into,'A',1)
-	}
-
 })(jQuery);
